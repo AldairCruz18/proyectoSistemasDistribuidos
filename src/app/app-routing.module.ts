c7e84f82a89930e1f831dea6cf9761653bc24312
Aldair Cruz <36124494+AldairCruz18@users.noreply.github.com>
2020-05-23 03:37:21 -0500

    Creación de preguntas
    Creación de la página preguntas, y adición del ruteo de la página

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { RegistroComponent } from './pages/registro/registro.component';
 import { LoginComponent } from './pages/login/login.component';
+import { PreguntasComponent } from './pages/preguntas/preguntas.component';
 
 const routes: Routes = [
   { path: 'home'    , component: HomeComponent },
   { path: 'registro', component: RegistroComponent },
   { path: 'login'   , component: LoginComponent },
+  { path: 'preguntas'   , component: PreguntasComponent },
   { path: '**', redirectTo: 'registro' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegistroComponent } from './pages/registro/registro.component';
+import { PreguntasComponent } from './pages/preguntas/preguntas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    RegistroComponent
+    RegistroComponent,
+    PreguntasComponent
   ],
   imports: [
     BrowserModule,
